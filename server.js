f0e27274c837192b6da2945569265d10efe78247
printshubh <printshub09@gmail.com>
2026-03-14 13:46:41 +0530

    server.js

--- server.js
+++ server.js
@@ -3,27 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve all static files from /public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const express = require('express');
-const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Serve static files from public/
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Home route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
   console.log('PrintsHubh running on port ' + PORT);
-});
-});
-
-app.listen(PORT, () => {
-  console.log('✅ PrintsHubh server running on port ' + PORT);
 });
